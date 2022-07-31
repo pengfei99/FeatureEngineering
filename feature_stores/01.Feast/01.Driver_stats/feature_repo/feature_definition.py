a3fff3f6195193f2b30e7cfd98e18939d8138627
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-07-31 15:04:19 +0200

    update

--- feature_stores/01.Feast/01.Driver_stats/feature_repo/feature_definition.py
+++ feature_stores/01.Feast/01.Driver_stats/feature_repo/feature_definition.py
@@ -9,7 +9,7 @@ from feast.types import Float32, Int64
 # production, you can use your favorite DWH, such as BigQuery. See Feast documentation
 # for more info.
 driver_hourly_stats = FileSource(
-    path="/feature_stores/01.Feast/01.Driver_stats/feature_repo/data/driver_stats.parquet",
+    path="/home/pliu/git/FeatureEngineering/feature_stores/01.Feast/01.Driver_stats/feature_repo/data/driver_stats.parquet",
     timestamp_field="event_timestamp",
     created_timestamp_column="created",
 )
@@ -23,7 +23,7 @@ driver = Entity(name="driver", join_keys=["driver_id"], value_type=ValueType.INT
 # data to our model online.
 driver_hourly_stats_view = FeatureView(
     name="driver_hourly_stats",
-    entities=["driver"],
+    entities=[driver],
     ttl=timedelta(days=1),
     schema=[
         Field(name="conv_rate", dtype=Float32),
